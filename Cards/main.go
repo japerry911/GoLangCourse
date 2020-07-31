9b7d4b8620a554716d4f5e28d5ad386b150c1297
japerry911 <jack.e.perry@outlook.com>
2020-07-31 13:45:13 -0600

    added in custom random and shuffle method

--- Cards/main.go
+++ Cards/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	cards := newDeck()
+	/*cards := newDeck()
 
 	hand, remainingDeck := deal(cards, 8)
 
@@ -12,6 +15,25 @@ func main() {
 
 	fmt.Println("Remaining Deck:")
 	remainingDeck.print()
+
+	fmt.Println(cards.toString())
+
+	filename, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	filename += "/Cards/CardsFileTest"
+
+	cards.saveToFile(filename)*/
+
+	filename, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	filename += "/Cards/CardsFileTest"
+	cardsFromFile := newDeckFromFile(filename)
+	cardsFromFile.shuffle()
+	cardsFromFile.print()
 }
 
 func newCard() string {
